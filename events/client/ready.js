691bcfd04f1de84acf129459f2b55ae96f1411d6
XeonE52680v3 <minh15052008@gmail.com>
2022-08-03 18:10:02 +0700

    Fix activity status

--- events/client/ready.js
+++ events/client/ready.js
@@ -17,16 +17,9 @@ module.exports = async (client) => {
 
 
     let guilds = client.guilds.cache.size;
-    let members = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0);
-    let channels = client.channels.cache.size;
 
-    const activities = [
-        `with ${guilds} servers! | /music radio`,
-        `with ${members} users! | /music play`,
-        `with ${channels} users! | /filter nightcore`
-    ]
-
-    setInterval(() => {
-        client.user.setActivity(`${activities[Math.floor(Math.random() * activities.length)]}`, { type: "STREAMING", url: "https://www.twitch.tv/lofichillnight" });
-    }, 60000)
+    client.user.setPresence({
+        activities: [{ name: `/music | ${guilds} servers`, type: 2 }],
+        status: 'online',
+    });
 };
